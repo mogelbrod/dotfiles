4d4b5de37340023ff01d2ed1c3dcff0b5b315803
Victor Hallberg <vigge19@gmail.com>
2009-12-06 15:05:01 +0100

    fish: Fixed svnlog function missing to specify --limit by default.

--- awesome/rc.lua
+++ awesome/rc.lua
@@ -155,7 +155,7 @@
 					awful.tag.viewonly(t)
 				end
 			end),
-		awful.button({ }, 3, awful.tag.viewtoggle),
+		awful.button({ }, 3, awful.tag.viewtoggle)
 		--awful.button({ }, 4, awful.tag.viewnext),
 		--awful.button({ }, 5, awful.tag.viewprev)
 	)
